12e23bcbe22c56ec07dbab92921d91318e4d7d42
Raquel Lugo <raquelarguedas11@gmail.com>
2016-07-22 10:41:08 -0600

    implementación del modelo, falta la excepción cuando hay un empate

--- src/Modelo/Modelo.java
+++ src/Modelo/Modelo.java
@@ -1,73 +1,159 @@
-
 package Modelo;
+
 import java.util.Observable;
 
 /**
  *
  * @author Raquel Lugo
  */
-public class Modelo extends Observable  {
-    
-    
-    public Modelo(){
-        for(int i=0; i<3; i++){
-          for(int j=0; j<3; j++){
-           tableroMatriz[i][j] = "";
-          }
+public class Modelo {
+
+    public Modelo() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                tableroMatriz[i][j] = "";
+            }
         }
     }
-    
-    public String movimiento(int pos){
-       
+
+    public String movimiento(int pos) {
+
         String marca = "";
-        
-        if(jugador == 1){
-          
+
+        if (jugador == 1) {
+            marca = marcarPosicion(pos, this.equiz);
+            jugador = 2;
+            if (obtenerGanador(this.tableroMatriz, this.equiz)) {
+                this.ganador = 1;
+            } //falta cuando no hay gane, empate
+
+        } else {
+
+            marca = marcarPosicion(pos, this.circulo);
+            jugador = 1;
+            if (obtenerGanador(this.tableroMatriz, this.circulo)) {
+                this.ganador = 2;
+            } //falta cuando no hay gane, empate
+
         }
-        
+
         return marca;
     }
-    
-    public String marcarPosicion(int pos, String marca){
-             
-          String aux_marca = "";
-          
-          switch(pos){
-          
-              case 1: break;
-              case 2: break;
-              case 3: break;
-              case 4: break;
-              case 5: break;
-              case 6: break;
-              case 7: break;
-              case 8: break;
-              case 9: break;
-               
-          }
-                  
-         return marca;
-    }
-    
-    public String validarPosicion(int i, int j, String marca){
-     
+
+    public String marcarPosicion(int pos, String marca) {
+
+        String aux_marca = "";
+
+        switch (pos) {
+            case 1:
+                validarPosicion(0, 0, marca);
+                break;
+            case 2:
+                validarPosicion(0, 0, marca);
+                break;
+            case 3:
+                validarPosicion(0, 0, marca);
+                break;
+            case 4:
+                validarPosicion(0, 0, marca);
+                break;
+            case 5:
+                validarPosicion(0, 0, marca);
+                break;
+            case 6:
+                validarPosicion(0, 0, marca);
+                break;
+            case 7:
+                validarPosicion(0, 0, marca);
+                break;
+            case 8:
+                validarPosicion(0, 0, marca);
+                break;
+            case 9:
+                validarPosicion(0, 0, marca);
+                break;
+        }
+
         return marca;
     }
-    
-    
-    public boolean obtenerGanador(){
-      
+
+    //Verifica que la casilla seleccionada este disponible
+    private String validarPosicion(int i, int j, String marca) {
+
+        String aux = "";
+        this.bandera_error = false;
+
+        if (this.tableroMatriz[i][j].equals("")) {
+            tableroMatriz[i][j] = marca;
+            aux = marca;
+        } else {
+            aux = this.tableroMatriz[i][j];
+            this.bandera_error = true;
+        }
+
+        return aux;
+    }
+
+    //Obtiene ganador cuando encuentra 3 en raya, en filas, colums o diagonales
+    public boolean obtenerGanador(String gato[][], String marca) {
+
+        int tamanio = gato.length;
+
+        for (int j = 0; j < tamanio; j++) {
+            int cont = 0;
+            for (int i = 0; i < tamanio; i++) {
+                if (gato[i][j].equals(marca)) {
+                    cont++;
+                }
+                if (cont == 3) {
+                    return true;
+                }
+            }
+        }
+
+        for (int i = 0; i < tamanio; i++) {
+            int cont = 0;
+            for (int j = 0; j < tamanio; j++) {
+                if (gato[i][j].equals(marca)) {
+                    cont++;
+                }
+                if (cont == 3) {
+                    return true;
+                }
+            }
+        }
+
+        for (int i = 0; i < tamanio; i++) {
+            int cont = 0;
+            for (int j = 0; j < tamanio; j++) {
+                if (i == j) {
+                    if (gato[i][j].equals(marca)) {
+                        cont++;
+                    }
+                    if (cont == 3) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        for (int i = tamanio; i > 0; i--) {
+            int cont = 0;
+            for (int j = tamanio; j > 0; j--) {
+                if (i == j) {
+                    if (gato[i][j].equals(marca)) {
+                        cont++;
+                    }
+                    if (cont == 3) {
+                        return true;
+                    }
+                }
+            }
+        }
+
         return true;
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
     public void setBandera_error(boolean bandera_error) {
         this.bandera_error = bandera_error;
     }
@@ -92,8 +178,8 @@ public class Modelo extends Observable  {
         return bandera_error;
     }
 
-    public int getJugador() {
-        return jugador;
+    public String getJugador() {
+        return (this.jugador == 1) ? "Primer Jugador 'X'" : "Segundo Jugador 'O'";
     }
 
     public int getGanador() {
@@ -107,13 +193,12 @@ public class Modelo extends Observable  {
     public String getCirculo() {
         return circulo;
     }
-    
-    
+
     private int ganador = 0;
     private String equiz = "X";
     private String circulo = "O";
     private boolean bandera_error = false;
     private String tableroMatriz[][];
     private int jugador = 1;
-    
+
 }
